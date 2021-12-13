43c57cda26d4f0681018bfac531a206c0bbf095e
Joe Jacobs - 2n1 <jjacobs43@gatech.edu>
2021-12-12 18:52:56 -0800

    adding teapot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -13,3 +14,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/teapot")
+def teapot():
+    return FileResponse('images/NPC_Tubby_Rank_7.png', status_code=418)
